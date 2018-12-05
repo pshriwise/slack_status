48a66749ab1facb44380358689298c3f30298730
Patrick Shriwise <pshriwise@gmail.com>
2018-12-05 18:18:00 +0000

    Cleaning up status format a bit.

--- status.py
+++ status.py
@@ -1,6 +1,6 @@
 
-argonne_office_status_dict = { "cnerg"    : ('anl', "at ANL, but available here!"),
-                               "openmc"   : ('desktop_computer', "at desk") }
+argonne_office_status_dict = { "cnerg"  : ('anl', "at ANL, but available here!"),
+                               "openmc" : ('desktop_computer', "at desk") }
 
 uw_nat_status_dict = { "cnerg"  : ('sweat', "exercising"),
                        "openmc" : ('uw', "in Madison") }
@@ -15,7 +15,7 @@ home_status_dict = { "cnerg"  : ('house', "at home"),
                      "openmc" : ('house', "at home") }
 
 lakeside_coffee_status_dict = { "cnerg" : ('coffee', "lakeside coffee"),
-                                "openmc" : ('uw', "in Madison") }
+                                "openmc" :('uw', "in Madison") }
 
 locations = { "argonne_office"  : argonne_office_status_dict,
               "uw_nat"          : uw_nat_status_dict,
